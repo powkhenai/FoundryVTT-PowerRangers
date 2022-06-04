e67a046cfb896aa1979789b62d903247926eca96
Jake Allen <jakedallen@me.com>
2022-06-04 02:09:56 -0400

    First commit on new repo forked from boilerplate system

---
+++ module/helpers/config.mjs
@@ -0,0 +1,26 @@
+export const POWERRANGERS = {};
+
+/**
+ * The set of Essence Scores used within the sytem.
+ * @type {Object}
+ */
+ POWERRANGERS.essences = {
+  "str": "POWERRANGERS.EssenceStr",
+  "spd": "POWERRANGERS.EssenceSpd",
+  "smt": "POWERRANGERS.EssenceSmt",
+  "soc": "POWERRANGERS.EssenceSoc"
+};
+
+POWERRANGERS.essenceAbbreviations = {
+  "str": "POWERRANGERS.EssenceStrAbbr",
+  "spd": "POWERRANGERS.EssenceSpdAbbr",
+  "smt": "POWERRANGERS.EssenceSmtAbbr",
+  "soc": "POWERRANGERS.EssenceSocAbbr"
+};
+
+POWERRANGERS.defenses = {
+    "tgh": "POWERRANGERS.DefenseTgh",
+    "eva": "POWERRANGERS.DefenseEva",
+    "wil": "POWERRANGERS.DefenseWil",
+    "clv": "POWERRANGERS.DefenseClv"
+};
