d3c6d4de1410cbe43ae6cdb5dc099a791058a7a6
Jake Allen <jakedallen@me.com>
2022-06-12 02:08:13 -0400

    WIP: Basics of hard coded specializations, currently fighting with handlebars

--- module/powerrangers.mjs
+++ module/powerrangers.mjs
@@ -67,6 +67,24 @@ Handlebars.registerHelper('toLowerCase', function(str) {
   return str.toLowerCase();
 });
 
+Handlebars.registerHelper('times', function(n, block) {
+    var accum = '';
+    for(var i = 0; i < n; ++i) {
+        block.data.index = i;
+        block.data.first = i === 0;
+        block.data.last = i === (n - 1);
+        accum += block.fn(this);
+    }
+    return accum;
+});
+
+Handlebars.registerHelper('for', function(from, to, incr, block) {
+    var accum = '';
+    for(var i = from; i < to; i += incr)
+        accum += block.fn(i);
+    return accum;
+});
+
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
